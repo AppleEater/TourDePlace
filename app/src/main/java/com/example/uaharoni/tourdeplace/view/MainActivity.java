ac654e6d308fb9ccab3309a41b34d3f5198c38cb
AppleEater <uaharoni@gmail.com>
2016-07-08 01:35:02 +0300

    Added snackbarMessageReceiver

--- app/src/main/java/com/example/uaharoni/tourdeplace/view/MainActivity.java
+++ app/src/main/java/com/example/uaharoni/tourdeplace/view/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.uaharoni.tourdeplace.view;
 
+import android.Manifest;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.preference.PreferenceManager;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,10 +20,12 @@ import android.util.Log;
 
 import com.example.uaharoni.tourdeplace.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener{
 
     private SharedPreferences sharedPreferences;
     private BroadcastReceiver snackBarMessageReceiver;
+    public static LocationManager locationManager;
+
 
 
 
@@ -39,6 +47,15 @@ public class MainActivity extends AppCompatActivity {
                 .registerReceiver(snackBarMessageReceiver, new IntentFilter("EVENT_SNACKBAR"));
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(snackBarMessageReceiver);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            locationManager.removeUpdates(this);
+        }
+    }
+
     private void initReceivers(){
         snackBarMessageReceiver = new BroadcastReceiver() {
             @Override
@@ -50,5 +67,21 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         };  // closing the receiver
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
     }
 }
