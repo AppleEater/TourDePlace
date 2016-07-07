427ae66f2f8055025ffb0ebc6b4149a4f27e6704
AppleEater <uaharoni@gmail.com>
2016-07-07 22:02:11 +0300

    Added ViewPagerAdapter

---
+++ app/src/main/java/com/example/uaharoni/tourdeplace/controller/ViewPagerAdapter.java
@@ -0,0 +1,39 @@
+package com.example.uaharoni.tourdeplace.controller;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+    private ArrayList<Fragment> fragments = new ArrayList<>();
+    private ArrayList<String> tabTitles = new ArrayList<>();
+
+    public ViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    public int addFragment(Fragment fragment, String tabTitle){
+        this.fragments.add(fragment);
+        this.tabTitles.add(tabTitle);
+        return (fragments.size()-1);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragments.get(position);
+    }
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if(position<fragments.size()){
+            return tabTitles.get(position);
+        } else {
+            return super.getPageTitle(position);
+        }
+    }
+    @Override
+    public int getCount() {
+        return fragments.size();
+    }
+}
